20404e275d4de3bca825ec5512d3966ab4dd6b6e
Phuong <moon.cantabella@gmail.com>
2021-10-21 08:51:00 +0300

    YesNo Quiz
    Build 10 yes/no quiz game

---
+++ 02 YNQuiz/main.py
@@ -0,0 +1,22 @@
+from data import question_data
+from question_model import Question
+from quiz_brain import QuizBrain
+
+# question_bank = []
+# for question in question_data:
+#     question_text = question["text"]
+#     question_answer = question["answer"]
+#     new_question = Question(question_text, question_answer)
+#     question_bank.append(new_question)
+
+question_bank = [Question(q["question"], q["correct_answer"]) for q in question_data]
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_question():
+    quiz.next_question()
+
+print("You're completed the quiz!")
+print(f"Your final score was {quiz.score}/{quiz.question_number}")
+
+
